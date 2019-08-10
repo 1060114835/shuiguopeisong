89ef1c9763ed4e817f8b5b2daa5a03e9be2b48c5
dongChen <1060114835@qq.com>
2019-08-10 11:24:26 +0800

    添加网络请求模块

--- app/src/main/java/com/example/fruitdelivery/common/net/Apis.java
+++ app/src/main/java/com/example/fruitdelivery/common/net/Apis.java
@@ -10,10 +10,10 @@ import retrofit2.http.GET;
  * 这个类存放Retrofit的Api接口，当后端的接口出来后我会补齐
  */
 public interface Apis {
-    String ARTICLE_URL = "article/list/1/json";
+    String articleUrl = "article/list/1/json";
     /**
      * 这个是网上的Api，这里只是拿来试验一下
      */
-    @GET(ARTICLE_URL)
+    @GET(articleUrl)
     Observable<JsonRootBean> getArticleCall();
 }
