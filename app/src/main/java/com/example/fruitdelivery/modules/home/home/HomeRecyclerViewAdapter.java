3ba8d421f2be81bc9b4653decf3374cede396f89
Richard <Richard1028283@outlook.com>
2019-08-13 11:54:34 +0800

    壳活动完成

--- app/src/main/java/com/example/fruitdelivery/modules/home/home/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/fruitdelivery/modules/home/home/HomeRecyclerViewAdapter.java
@@ -3,7 +3,15 @@ package com.example.fruitdelivery.modules.home.home;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.fruitdelivery.R;
 
 import java.util.List;
 
@@ -32,13 +40,48 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
-
+        if (i == TYPE_BANNER) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_home,
+                    viewGroup,false);
+            return new BannerViewHolder(view);
+        }else if (i == TYPE_FUNCTION) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_function,
+                    viewGroup,false);
+            return new FunctionViewHolder(view);
+        }else if (i == TYPE_SELLERS) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_sellers,
+                    viewGroup,false);
+            return new SellersViewHolder(view);
+        }else if (i == TYPE_FRUIT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_fruit,
+                    viewGroup,false);
+            return new FruitViewHolder(view);
+        }else if (i == TYPE_FOOTER) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_footer,
+                    viewGroup,false);
+            return new FooterViewHolder(view);
+        }
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
 
+        if (viewHolder instanceof BannerViewHolder) {
+
+        }else if (viewHolder instanceof FunctionViewHolder) {
+
+        }else if (viewHolder instanceof SellersViewHolder) {
+
+        }else if (viewHolder instanceof FruitViewHolder) {
+
+            FruitViewHolder fruitViewHolder = (FruitViewHolder)viewHolder;
+            Glide.with(context).load(urlList.get(i)).into(fruitViewHolder.imageView);
+
+        }else if (viewHolder instanceof FooterViewHolder) {
+            FooterViewHolder footerViewHolder = (FooterViewHolder)viewHolder;
+
+        }
     }
 
     @Override
@@ -54,27 +97,54 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
             return TYPE_FUNCTION;
         }else if (position == 2) {
             return TYPE_SELLERS;
-        }else if (position + 1 == getItemCount() ) {
+        }else if (position + 1 == getItemCount()) {
             return TYPE_FOOTER;
         }else {
             return TYPE_FRUIT;
         }
     }
 
-    class FunctionViewHolder {
+    class BannerViewHolder extends RecyclerView.ViewHolder {
 
+        public BannerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 
-    class SellersViewHolder {
+    class FunctionViewHolder extends RecyclerView.ViewHolder {
 
+        public FunctionViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 
-    class FruitViewHolder {
+    class SellersViewHolder extends RecyclerView.ViewHolder {
 
+        public SellersViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 
-    class FooterViewHolder {
+    class FruitViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imageView;
+
+        public FruitViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.img_home_fruit);
+        }
+    }
+
+    class FooterViewHolder extends RecyclerView.ViewHolder{
+
+        ProgressBar progressBar;
+        TextView textView;
+
+        public FooterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.pb_home_footer);
+            textView = itemView.findViewById(R.id.tv_home_footer);
+        }
     }
 
 }
--- app/src/main/java/com/example/fruitdelivery/modules/home/shell/ShellActivity.java
+++ app/src/main/java/com/example/fruitdelivery/modules/home/shell/ShellActivity.java
@@ -1,15 +1,25 @@
 package com.example.fruitdelivery.modules.home.shell;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
 import com.example.fruitdelivery.R;
 import com.example.fruitdelivery.base.BaseActivity;
 import com.example.fruitdelivery.base.BaseActivityWithToolbar;
+import com.example.fruitdelivery.modules.home.home.HomeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 壳活动，应该包含 我的、订单、首页 三个碎片
  */
 public class ShellActivity extends BaseActivityWithToolbar<ShellPresenter> implements IShellView {
+
+    private static List<Fragment> fragmentList;
 
     @Override
     protected int getContentLayoutId() {
@@ -23,5 +33,22 @@ public class ShellActivity extends BaseActivityWithToolbar<ShellPresenter> imple
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+
+        fragmentList = new ArrayList<>();
+        HomeFragment homeFragment = new HomeFragment();
+
+        fragmentList.add(homeFragment);
+
+        ViewPager viewPager = findViewById(R.id.shell_view_pager);
+        ShellAdapter shellAdapter = new ShellAdapter(getSupportFragmentManager(),fragmentList);
+        viewPager.setAdapter(shellAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.shell_tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("首页").setIcon(R.drawable.shell_tab_homepage));
+        tabLayout.addTab(tabLayout.newTab().setText("订单").setIcon(R.drawable.shell_tab_order));
+        tabLayout.addTab(tabLayout.newTab().setText("我的").setIcon(R.drawable.shell_tab_mine));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
     }
+
 }
