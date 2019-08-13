93f3b6899564cfc9793e2c15a5059ae52b0ed69c
coder-pig <your email>
2019-08-13 10:14:32 +0800

    Register

--- app/src/main/java/com/example/fruitdelivery/modules/account/register/Test.java
+++ app/src/main/java/com/example/fruitdelivery/modules/account/register/Test.java
@@ -1,26 +1,15 @@
 package com.example.fruitdelivery.modules.account.register;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import com.example.fruitdelivery.R;
-import com.example.fruitdelivery.base.BaseActivity;
-import com.example.fruitdelivery.base.BasePresenter;
+
 
 /**
  * @author dyy15
