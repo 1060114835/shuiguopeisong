0931c0de6f047fc0ad6420745b15d6ab72795536
Richard <Richard1028283@outlook.com>
2019-08-14 23:40:47 +0800

    修复Bug，使用本地数据进行加载，增加各种方法

--- app/src/main/java/com/example/fruitdelivery/modules/home/home/HomeViewPagerAdapter.java
+++ app/src/main/java/com/example/fruitdelivery/modules/home/home/HomeViewPagerAdapter.java
@@ -2,6 +2,7 @@ package com.example.fruitdelivery.modules.home.home;
 
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -21,10 +22,13 @@ public class HomeViewPagerAdapter extends PagerAdapter {
 
     public HomeViewPagerAdapter(List<ImageView> imageViewList) {
         this.imageViewList = imageViewList;
+        Log.d("TAG","ViewPager的Adapter构造方法得到执行");
     }
 
     @Override
     public int getCount() {
+        Log.d("TAG","getCount()方法得到执行");
+
         return Integer.MAX_VALUE;
     }
 
@@ -36,6 +40,8 @@ public class HomeViewPagerAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        Log.d("TAG","instantiateItem方法得到执行");
 
         position %= imageViewList.size();
         if (position < 0) {
@@ -51,4 +57,8 @@ public class HomeViewPagerAdapter extends PagerAdapter {
         return imageView;
     }
 
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+    }
 }
